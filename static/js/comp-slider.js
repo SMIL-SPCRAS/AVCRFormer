3346f34b7143dac18bb48db37371ae07a20b6dd6
Alex <36799721+MiSTeR1995@users.noreply.github.com>
2024-02-01 14:45:39 +0300

    slider update

--- static/js/comp-slider.js
+++ static/js/comp-slider.js
@@ -1,8 +1,12 @@
-const slider_first = document.querySelector('.slider-first');
-const slider_second = document.querySelector('.slider-second');
-const slider_third = document.querySelector('.slider-third');
 const btn = document.querySelector('#btt-button');
+const bttArr = document.querySelector('#btt-arr');
+const sliderItems = document.querySelectorAll('.slider__item');
+const sliderLine = document.querySelector('.slider__line');
+const sliderDots = document.querySelectorAll('.slider__dot');
+const sliderBtnNext = document.querySelector('.slider__btn-next');
+const sliderBtnPrev = document.querySelector('.slider__btn-prev');
 
+// Появление стрелочки навверх в середине страницы
 window.addEventListener('scroll', function () {
     if (window.scrollY > 100) {
         btn.classList.add('show');
@@ -11,48 +15,74 @@ window.addEventListener('scroll', function () {
     }
 });
 
+btn.addEventListener('mouseover', function (e) {
+    bttArr.classList.add("btt-hover");
+});
 
-let slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-    let i;
-    const slides = document.getElementsByClassName('mySlides');
-    const dots = document.getElementsByClassName('dot');
-    if (n > slides.length) {
-        slideIndex = 1;
-    }
-    if (n < 1) {
-        slideIndex = slides.length;
-    }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = 'none';
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(' active', '');
-    }
-    slides[slideIndex - 1].style.display = 'block';
-    dots[slideIndex - 1].className += ' active';
-}
+btn.addEventListener('mouseleave', function (e) {
+    e.preventDefault();
+    bttArr.classList.remove('btt-hover');
+});
 
 btn.addEventListener('click', function (e) {
     e.preventDefault();
     window.scrollTo({ top: 0, behavior: 'smooth' });
 });
 
+let sliderCount = 0;
+let sliderWidth;
 
+// адаптивность
+window.addEventListener('resize', showSlide);
+
+function showSlide() {
+    sliderWidth = document.querySelector('.slider').offsetWidth;
+    sliderLine.style.width = sliderWidth * sliderItems.length + 'px';
+    sliderItems.forEach(item => item.style.width = sliderWidth + 'px');
+
+    rollSlider();
+}
+
+showSlide();
+
+function nextSlide() {
+    sliderCount++;
+    if (sliderCount >= sliderItems.length) sliderCount = 0;
+
+    rollSlider();
+    thisSlide(sliderCount);
+}
+
+function prevSlide() {
+    sliderCount--;
+    if (sliderCount < 0) sliderCount = sliderItems.length - 1;
+
+    rollSlider();
+    thisSlide(sliderCount);
+
+}
+
+sliderBtnNext.addEventListener('click', nextSlide);
+sliderBtnPrev.addEventListener('click', prevSlide);
+
+function rollSlider() {
+    sliderLine.style.transform = `translateX(${-sliderCount * sliderWidth}px)`;
+}
+
+function thisSlide(index) {
+    sliderDots.forEach(item => item.classList.remove('active-dot'));
+    sliderDots[index].classList.add('active-dot')
+}
+
+// клик на dots
+sliderDots.forEach((dot,index) => {
+    dot.addEventListener('click', () => {
+        sliderCount = index;
+        rollSlider();
+        thisSlide(sliderCount);
+    })
+})
 console.clear();
-
 const checkboxes = document.querySelectorAll('input[type="checkbox"]');
 
 setTimeout(function () {
